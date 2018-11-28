2233890d33d3f93f09ded9cb0685c1920106ab44
ns248s <ns248s@att.com>
2018-11-28 16:49:08 +0530

    Single Page Applications Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 const DISH = {
   id: '0',
@@ -50,11 +53,20 @@ const DISH = {
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-    @Input()
     dish: Dish;
-  constructor() { }
-  
+
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
+
   ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    console.log('id', typeof id);
+    this.dish = this.dishservice.getDish(id);
+    console.log('dish==>', this.dish   );
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
